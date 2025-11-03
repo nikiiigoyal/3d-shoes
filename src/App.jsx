0fa84a2def18a032c7f1242c3a12526bc65e5f36
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-03 13:19:48 +0530

    initial commir

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from 'react'
+import './App.css'
+import { OrbitControls,useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+function App() {
+
+
+  return (
+    <>
+      <div className='app'>
+            <div class="wrapper">
+        <div className="card">
+            <div class="product-canvas">
+          <Canvas >
+
+
+          </Canvas>
+
+            </div>
+            <h2>Color chooser</h2>
+            <div className='colors'>
+                 <div>
+                    <input type="color" id="head" name="head"
+                           value="#e66465" />
+                    <label for="head">Main</label>
+                  </div>
+
+                <div>
+                    <input type="color" id="body" name="body"
+                            value="#f6b73c" />
+                    <label for="body">Stripes</label>
+                </div>
+                 <div>
+                    <input type="color" id="body" name="body"
+                            value="#f6b73c" />
+                    <label for="body">Soul</label>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
